503d86fb3e4cd41c88772a73cf4247adf9c77d4d
bgayman <bgayman@apppartner.com>
2017-09-12 09:31:05 -0400

    Add possible station descriptions

--- BikeShare/BikeStation.swift
+++ BikeShare/BikeStation.swift
@@ -15,6 +15,50 @@ import CoreText
 
 struct BikeStation
 {
+    static let greenDescriptions = ["Get ready to break the soundbearier!!",
+                                    "Beary nice place to visit!",
+                                    "Grab it with your bear hands and get going!",
+                                    "Grrrrreat news!!!",
+                                    "You have the right to bear bikes.",
+                                    "Go bearzerk 🐻!!",
+                                    "This is paws-ibly the best station you'll see all day.",
+                                    "This station: a Kodiak moment!!",
+                                    "A real honey hole!!",
+                                    "I hope you're paws-itively ready, because this station is!",
+                                    "This station has the bear necessities.",
+                                    "Think you've seen best station? Hold my bear.",
+                                    "Beared you go again!!",
+                                    "Feel the bearn!!",
+                                    
+                                    ]
+    
+    static let orangeDescriptions = ["Bear with me now, there might be what you need at this station.",
+                                     "Bearly any left, but have at it.",
+                                     "This might give you paws, but this station is just looking Ok.",
+                                     "Just so you have your bearings, this station might not have what you need when you get there.",
+                                     "Bear-leave strong enough, and this station might work for you.",
+                                     "Paws before you go here.",
+                                     "Things are about to get hairy.",
+                                     "Feeling clawstrophobic about this station.",
+                                     "You'll have to grin and bear it for this one.",
+                                     "Bear down and get to this station quick.",
+                                     "No time to take a paws!!",
+                                     
+                                     ]
+    
+    static let redDescriptions = ["This is Unbearable!!🐻!!!!",
+                                  "Bad news bear!!🐻!!",
+                                  "This station is polarizing.",
+                                  "Impawsible!",
+                                  "A-bear-antly this station is having a bad day!",
+                                  "Well this looks grizzly for you",
+                                  "Panda-monium!!🐼",
+                                  "Hope you can claw your way out of this one!",
+                                  "Beary bad news!!",
+                                  "Want a hug?",
+                                  "InClawsible!!"
+                                  ]
+    
     let name: String
     let timestamp: Date
     let coordinates: CLLocationCoordinate2D
